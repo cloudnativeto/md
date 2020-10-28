e0db31bb4926489e06fdccaac27fa181bc88c0c8
Jimmy Song <rootsongjc@gmail.com>
2020-10-28 20:42:50 +0800

    update dialog

--- src/assets/scripts/default-content.js
+++ src/assets/scripts/default-content.js
@@ -2,7 +2,7 @@ const DEFAULT_CONTENT = `# 云原生社区公众号模板
 
 ### 关于云原生社区
 
-![](https://tva1.sinaimg.cn/large/0081Kckwly1gk5ac6i88mj30ms0mgq3u.jpg)
+![](https://tva1.sinaimg.cn/large/0081Kckwly1gk5bzvrslej30p00dwgmr.jpg)
 
 云原生社区是一个中立的云原生终端用户社区，由 CNCF 大使、开源意见领袖共同发起成立于 2020 年 5 月 12 日，旨在推广云原生技术，构建开发者生态。
 
