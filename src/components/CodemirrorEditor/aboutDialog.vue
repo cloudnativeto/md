7e85661d9c76300de89d1f67f46f84c87c302475
Jimmy Song <rootsongjc@gmail.com>
2020-10-28 19:45:34 +0800

    update default tempalte and dialog

--- src/components/CodemirrorEditor/aboutDialog.vue
+++ src/components/CodemirrorEditor/aboutDialog.vue
@@ -8,27 +8,27 @@
         center
     >
         <div style="text-align: center">
-            <h3>一款高度简洁的微信 Markdown 编辑器</h3>
+            <h3>云原生社区公众号 Markdown 编辑器</h3>
         </div>
         <div style="text-align: center; margin-top: 10px">
-            <p>扫码关注我的公众号，原创技术文章第一时间推送！</p>
+            <p>扫码关注云原生社区的公众号，获取云原生最新资讯！</p>
             <img
-                src="https://gitee.com/yanglbme/resource/raw/master/doocs-md/qrcode.png"
+                src=""https://cloudnative.to/images/wechat-qrcode.png
                 style="width: 40%; display: block; margin: 20px auto 10px"
             />
         </div>
         <span slot="footer" class="dialog-footer">
             <el-button
                 type="primary"
-                @click="onRedirect('https://github.com/doocs/md')"
+                @click="onRedirect('https://github.com/cloudnativeto/md')"
                 plain
                 >GitHub 仓库</el-button
             >
             <el-button
                 type="primary"
-                @click="onRedirect('https://gitee.com/doocs/md')"
+                @click="onRedirect('https://cloudnative.to')"
                 plain
-                >Gitee 仓库</el-button
+                >云原生社区</el-button
             >
         </span>
     </el-dialog>
